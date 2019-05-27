23c530bc0f4216638515117cd8e5d295abbc22f5
ricard.arman.kuodis <ricard.arman.kuodis@accenture.com>
2019-05-27 17:37:11 +0300

    Finished tutorial

---
+++ langaw/lib/components/backyard.dart
@@ -0,0 +1,27 @@
+import 'dart:ui';
+
+import 'package:flame/sprite.dart';
+import 'package:langaw/langaw-game.dart';
+
+class Backyard {
+  final LangawGame game;
+  Sprite bgSprite;
+  Rect bgRect;
+
+  Backyard(this.game) {
+    bgSprite = Sprite("bg/backyard.png");
+    // 9 tiles wide, 23 tiles high
+    bgRect = Rect.fromLTWH(
+      0,
+      game.screenSize.height - (game.tileSize * 23),
+      game.tileSize * 9,
+      game.tileSize * 23,
+    );
+  }
+
+  void render(Canvas c) {
+    bgSprite.renderRect(c, bgRect);
+  }
+
+  void update(double t) {}
+}
